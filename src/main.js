106b8ebe18300745ceb78c1cae3d2d702cbcfdce
akira-cn <akira.cn@gmail.com>
2018-11-21 17:02:12 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { Vue } from 'sprite-vue';
+import { Vue, spritejs } from 'sprite-vue';
+import { install } from 'sprite-extend-shapes';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+spritejs.use(install);
+console.log(spritejs);
 
 /* eslint-disable no-new */
 new Vue({
